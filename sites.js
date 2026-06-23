16f1a18c5afdb6e1d3883e8d873943485b1b8488
RickDevR <davidrosenberg545@gmail.com>
2026-06-23 19:23:02 -0400

    Update sites.js

--- sites.js
+++ sites.js
@@ -6,6 +6,7 @@ Task challenge | https://rickdevr.github.io/Task/
 Duck clicker | https://rickdevr.github.io/Duck-clicker/
 Secret coding language decode and Ecode | https://rickdevr.github.io/Secretcoder/
 Satisfying squishy sound | https://rickdevr.github.io/squishy/
+chat room | https://rickdevr.github.io/test/
 `.trim().split("\n").map(line => {
     const [name, url] = line.split("|").map(x => x.trim());
     return { name, url };
