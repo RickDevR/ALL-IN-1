1b1e5d0d9b2cee32e65ed5bac6b8c47880311f41
RickDevR <davidrosenberg545@gmail.com>
2026-06-23 14:59:23 -0400

    Update sites list with new website entries
    Added new website entries to the sites list.

--- sites.js
+++ sites.js
@@ -4,6 +4,8 @@
 const sites = `
 Task challenge | https://rickdevr.github.io/Task/
 Duck clicker | https://rickdevr.github.io/Duck-clicker/
+Secret coding language decode and Ecode | https://rickdevr.github.io/Secretcoder/
+Satisfying squishy sound | https://rickdevr.github.io/squishy/
 `.trim().split("\n").map(line => {
     const [name, url] = line.split("|").map(x => x.trim());
     return { name, url };
