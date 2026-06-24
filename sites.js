f7711cfc65329fdb2a0c19a14cfcce81b87c9d0d
RickDevR <davidrosenberg545@gmail.com>
2026-06-24 17:14:49 -0400

    Add 'Crush test' to the website list
    Added a new site 'Crush test' to the website list.

--- sites.js
+++ sites.js
@@ -6,7 +6,7 @@ Task challenge | https://rickdevr.github.io/Task/
 Duck clicker | https://rickdevr.github.io/Duck-clicker/
 Secret coding language decode and Ecode | https://rickdevr.github.io/Secretcoder/
 Satisfying squishy sound | https://rickdevr.github.io/squishy/
-mini chat room | https://rickdevr.github.io/mini-chat/
+Crush test| https://rickdevr.github.io/Testing-website/
 `.trim().split("\n").map(line => {
     const [name, url] = line.split("|").map(x => x.trim());
     return { name, url };
